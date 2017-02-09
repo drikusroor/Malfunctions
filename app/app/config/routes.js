6487636a913f852908ab8300dad8b295be6b3211
Roor <droor@acto.nl>
2017-02-09 23:08:14 +0100

    Deleted datasource from routes.ts

--- app/app/config/routes.js
+++ app/app/config/routes.js
@@ -61,13 +61,5 @@ angular.module('StoringenApp')
                 }
             },
             resolve: {}
-        })
-            .state('portal.admin.datasourceManagement.detail', {
-            url: '/:id',
-            views: {
-                'content@': {
-                    templateUrl: 'app/components/datasource-management/detail/datasource-detail.html',
-                }
-            }
         });
     }]);
