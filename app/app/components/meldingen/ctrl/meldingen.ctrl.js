a78b2b6341671a433d0efe8255e65a3d5d7b5fc4
Roor <droor@acto.nl>
2017-02-09 23:01:51 +0100

    Improved the status page and paved the ground for meldingen

--- app/app/components/meldingen/ctrl/meldingen.ctrl.js
+++ app/app/components/meldingen/ctrl/meldingen.ctrl.js
@@ -2,43 +2,25 @@ var StoringenApp;
 (function (StoringenApp) {
     "use strict";
     var MeldingenCtrl = (function () {
-        function MeldingenCtrl($scope, $http, $state, $window, $document, GEBOUWEN) {
-            var _this = this;
+        function MeldingenCtrl($scope, $http, $state, $window, $document, GebouwenService) {
             this.$scope = $scope;
             this.$http = $http;
             this.$state = $state;
             this.$window = $window;
             this.$document = $document;
-            this.GEBOUWEN = GEBOUWEN;
-            this.selectGebouw = function (id) {
-                _this.selectedGebouw = _this.GEBOUWEN.find(function (g) { return g.gebouwId === id; });
-                _this.form.adres = _this.selectedGebouw.adres;
-                _this.form.vhenr = _this.selectedGebouw.vhenr;
-            };
-            this.initMap = function () {
-                var coordinates = {
-                    lat: 52.1646443,
-                    lng: 5.363390400000071
-                };
-                _this.$window.map = new google.maps.Map(document.getElementById('map'), {
-                    center: coordinates,
-                    zoom: 16
-                });
-            };
-            this.gebouwen = GEBOUWEN;
-            this.selectedTab = "map";
-            this.form = {};
-            var that = this;
-            this.$document.ready(function () {
-                that.initMap();
+            this.GebouwenService = GebouwenService;
+            var id = $state.params.id;
+            GebouwenService.getGebouwById(id).then(function (data) {
+                this.gebouw = data;
+                this.meldingen = data.meldingen;
             });
         }
         return MeldingenCtrl;
     }());
-    MeldingenCtrl.$inject = ['$scope', '$http', '$state', '$window', '$document', 'GEBOUWEN'];
+    MeldingenCtrl.$inject = ['$scope', '$http', '$state', '$window', '$document', 'GebouwenService'];
     StoringenApp.MeldingenCtrl = MeldingenCtrl;
-    function controller($scope, $http, $state, $window, $document, GEBOUWEN) {
-        return new MeldingenCtrl($scope, $http, $state, $window, $document, GEBOUWEN);
+    function controller($scope, $http, $state, $window, $document, GebouwenService) {
+        return new MeldingenCtrl($scope, $http, $state, $window, $document, GebouwenService);
     }
     angular.module('StoringenApp').controller('MeldingenCtrl', controller);
 })(StoringenApp || (StoringenApp = {}));
